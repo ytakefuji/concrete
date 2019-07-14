fcd7ddba1035333335f3ab14229776284a7144d4
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2019-07-14 15:50:35 +0900

    Add files via upload

--- concrete_rf.py
+++ concrete_rf.py
@@ -6,15 +6,15 @@ with open("test.csv",'r') as f, open("newtest.csv",'w') as f1:
  for line in f:
   f1.write(line)
 conc=pd.read_csv('newtest.csv')
-conc.columns=['num','cement','blast','ash','water','sp','cagg','fagg','age','strength']
-from sklearn.model_selection import train_test_split
+conc.columns=[',','cement','blast','ash','water','sp','cagg','fagg','age','strength']
+conc[conc.columns[1:10]].to_csv('concrete.csv')
 y=conc['strength']
-X=conc.drop(['strength','num'],axis=1)
+X=conc.drop(['strength',','],axis=1)
+from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=54,shuffle=True)
 from sklearn.ensemble import RandomForestRegressor
-clf=RandomForestRegressor(n_estimators=382, max_depth=None,min_samples_split=2,random_state=8)
+clf=RandomForestRegressor(n_estimators=382, max_depth=None,min_samples_split=2,random_state=7)
 clf.fit(X_train,y_train)
-y_pred=clf.predict(X_test)
 print(clf.score(X_test,y_test))
 dic=dict(zip(X.columns,clf.feature_importances_))
 for item in sorted(dic.items(), key=lambda x: x[1], reverse=True):
