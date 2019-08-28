f13f8767c2a13f24f3414e89c6879a3765feb63f
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2019-08-28 23:41:15 +0900

    Add files via upload

--- lgbm_crackGPU.py
+++ lgbm_crackGPU.py
@@ -9,6 +9,10 @@ udy=np.zeros(2025)
 ud=genfromtxt('ud_gray.csv',delimiter=',')
 print('loading ud_gray.csv is completed.')
 X=np.concatenate((cd,ud))
+n=128.0
+X=np.asarray(X)/n
+X=np.subtract(X,255.0/n/2.0+0.5)
+
 y=np.concatenate((cdy,udy))
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=90,shuffle=True)
@@ -36,10 +40,16 @@ params = {
 
 gbm = lgb.train(params,
                 lgb_train,
-                num_boost_round=99,
+                num_boost_round=999,
                 valid_sets=lgb_eval,
                 verbose_eval=1,
-                early_stopping_rounds=5)
+                early_stopping_rounds=555)
 y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
-from sklearn.metrics import roc_auc_score
-print("%.4f"%roc_auc_score(y_test,y_pred))
+pred=[]
+for i in y_pred:
+ if i>0.5: pred.append(1)
+ else:pred.append(0)
+y_pred=pred
+#from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
+print("%.4f"%accuracy_score(y_test,y_pred))
